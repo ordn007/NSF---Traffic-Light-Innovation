99c738f19b0df688dc265f4906595dca60f035d9
Oredn Aitchedji <oaitchedji@mines.edu>
2018-06-18 11:27:18 -0600

    LCD & Prototype code

---
+++ Prototype.ino
@@ -0,0 +1,135 @@
+#include <Wire.h> 
+#include <LiquidCrystal_I2C.h>
+
+LiquidCrystal_I2C lcd(0x27,20,4); 
+
+int red = 13;
+int red1 = 1;
+int red2 = 2;
+int red3 = 3;
+int red4 = 4;
+int red5 = 5;
+int red6 = 6;
+
+int yellow = 12;
+int green = 11;
+int Turn = 0;
+
+void setup() 
+{
+  lcd.backlight();
+  lcd.init();
+
+  pinMode(red, OUTPUT);
+  pinMode(yellow, OUTPUT);
+  pinMode(green, OUTPUT);
+}
+
+void loop()
+{
+//-------- Write characters on the display ------------------
+  // NOTE: Cursor Position: Lines and Characters start at 0  
+  lcd.setCursor(0,0); //Start at character 4 on line 0
+  lcd.print("Time:");
+  //delay(1000);
+  lcd.setCursor(0,1);
+  lcd.print("Today's Date");
+  //delay(1000);  
+  lcd.setCursor(2,2);
+  lcd.print("**Informations**");
+  lcd.setCursor(15,3);
+  //delay(1000);   
+  lcd.print("T-LIG");
+  //delay(2000);
+  
+  //changeLights();
+  // green off, yellow off, red ON on for 7 seconds
+  digitalWrite(red, HIGH);
+  digitalWrite(red1, HIGH);
+  digitalWrite(red2, HIGH);
+  digitalWrite(red3, HIGH);
+  digitalWrite(red4, HIGH);
+  digitalWrite(red5, HIGH);
+  digitalWrite(red6, HIGH);
+  digitalWrite(yellow, LOW);
+  digitalWrite(green, LOW);
+  delay(1000);
+digitalWrite(red, HIGH);
+  digitalWrite(red1, HIGH);
+  digitalWrite(red2, HIGH);
+  digitalWrite(red3, HIGH);
+  digitalWrite(red4, HIGH);
+  digitalWrite(red5, HIGH);
+  digitalWrite(red6, LOW);
+  digitalWrite(yellow, LOW);
+  digitalWrite(green, LOW);
+  delay(1000);
+digitalWrite(red, HIGH);
+  digitalWrite(red1, HIGH);
+  digitalWrite(red2, HIGH);
+  digitalWrite(red3, HIGH);
+  digitalWrite(red4, HIGH);
+  digitalWrite(red5, LOW);
+  digitalWrite(red6, LOW);
+  digitalWrite(yellow, LOW);
+  digitalWrite(green, LOW);
+  delay(1000);
+digitalWrite(red, HIGH);
+  digitalWrite(red1, HIGH);
+  digitalWrite(red2, HIGH);
+  digitalWrite(red3, HIGH);
+  digitalWrite(red4, LOW);
+  digitalWrite(red5, LOW);
+  digitalWrite(red6, LOW);
+  digitalWrite(yellow, LOW);
+  digitalWrite(green, LOW);
+  delay(1000);
+digitalWrite(red, HIGH);
+  digitalWrite(red1, HIGH);
+  digitalWrite(red2, HIGH);
+  digitalWrite(red3, LOW);
+  digitalWrite(red4, LOW);
+  digitalWrite(red5, LOW);
+  digitalWrite(red6, LOW);
+  digitalWrite(yellow, LOW);
+  digitalWrite(green, LOW);
+  delay(1000);
+digitalWrite(red, HIGH);
+  digitalWrite(red1, HIGH);
+  digitalWrite(red2, LOW);
+  digitalWrite(red3, LOW);
+  digitalWrite(red4, LOW);
+  digitalWrite(red5, LOW);
+  digitalWrite(red6, LOW);
+  digitalWrite(yellow, LOW);
+  digitalWrite(green, LOW);
+  delay(1000);
+digitalWrite(red, HIGH);
+  digitalWrite(red1, LOW);
+  digitalWrite(red2, LOW);
+  digitalWrite(red3, LOW);
+  digitalWrite(red4, LOW);
+  digitalWrite(red5, LOW);
+  digitalWrite(red6, LOW);
+  digitalWrite(yellow, LOW);
+  digitalWrite(green, LOW);
+  delay(1000);
+
+
+  // turn off red, then turn green ON for 5 seconds
+  digitalWrite(red, LOW);
+  digitalWrite(green, HIGH);
+  delay(5000);
+
+  // turn green off, then turn yellow ON for 2 seconds
+  digitalWrite(yellow, HIGH);
+  digitalWrite(green, LOW);
+  delay(2000);
+
+}
+
+/*void changeLights() {
+  
+
+}
+*/
---
+++ Timer_prototype.ino
@@ -0,0 +1,80 @@
+unsigned long start, finished, elapsed;
+int start_bro = 1;
+
+#include <Wire.h> 
+#include <LiquidCrystal_I2C.h>
+
+LiquidCrystal_I2C lcd(0x27,20,4);  // set the LCD address to 0x20 for a 16 chars and 2 line display
+ 
+void setup()
+{
+  lcd.init();                      // initialize the lcd 
+  lcd.backlight();
+  Serial.begin(9600);
+}
+ 
+void displayResult()
+{
+  int h,m,s,ms;
+  unsigned long over;
+  elapsed=finished-start;
+  h=int(elapsed/3600000);
+  over=elapsed%3600000;
+  m=int(over/60000);
+  if (m < 10) {
+    lcd.setCursor(7,3);
+    lcd.print(" ");
+  }
+  over=over%60000;
+  s=int(over/1000);
+  if (s < 10) {
+    lcd.setCursor(10,2);
+    lcd.print(" ");
+  }
+  ms=over%1000;
+  /*
+  Serial.print("Raw elapsed time: ");
+  Serial.println(elapsed);
+  Serial.print("Elapsed time: ");
+  Serial.print(h,0);
+  Serial.print("h ");
+  Serial.print(m,0);
+  Serial.print("m ");
+  Serial.print(s,0);
+  Serial.print("s ");
+  Serial.print(ms,0);
+  Serial.println("ms");
+  Serial.println();
+  */
+  lcd.setCursor(0,0);
+  lcd.print("hours: ");
+  lcd.print(h);
+  lcd.setCursor(0,1);
+  lcd.print("mins: ");
+  lcd.print(m);
+  lcd.setCursor(0,2);
+  lcd.print("seconds: ");
+  lcd.print(s);
+  lcd.setCursor(0,3);
+  lcd.print("millis: ");
+  lcd.print(ms);
+  
+}
+ 
+void loop()
+{
+  if (start_bro==1)
+  {
+    start=millis();
+    delay(200); // for debounce
+    Serial.println("Started...");
+    start_bro = 0;
+  }
+  
+  if (1==HIGH)
+  {
+    finished=millis();
+    displayResult();
+    delay(70);
+  }
+}
